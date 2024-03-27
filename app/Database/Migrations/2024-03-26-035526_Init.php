a9e95cf3bf7f384f54aa19241a6a6c28ea41e548
ANGGA <anggasap2210@gmail.com>
2024-03-27 08:29:24 +0700

    migration

---
+++ app/Database/Migrations/2024-03-26-035526_Init.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Init extends Migration
+{
+    public function up()
+    {
+        // Table jenis_layanan
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'bobot' => [
+                'type' => 'INT',
+                'constraint' => 5,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('jenis_layanan');
+
+        // Table jenis_urgensi
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'bobot' => [
+                'type' => 'INT',
+                'constraint' => 5,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('jenis_urgensi');
+
+        // Table layanan
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'jenis_layanan_id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+            ],
+            'jenis_urgensi_id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+            ],
+            'status' => [
+                'type' => 'INT',
+                'constraint' => 1,
+            ],
+            'nilai_bobot' => [
+                'type' => 'INT',
+                'constraint' => 5,
+            ],
+            'prioritas' => [
+                'type' => 'INT',
+                'constraint' => 5,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+    }
+
+    public function down()
+    {
+        //
+    }
+}
